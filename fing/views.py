d48da8b01e70688733f73769db99d80415d11542
fing520 <fing@easymail.com.cn>
2015-08-03 17:59:02 +0800

    del

--- fing/views.py
+++
@@ -1,5 +0,0 @@
-from django.template import Context,loader
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
-def home(request):    
-    return render_to_response('myip/home.html')
